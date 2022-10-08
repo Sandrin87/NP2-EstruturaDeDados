95747510aa2d8bd87b91951ba372ab5947d6d089
Vitoria Souza <vitoriasouzza.vrs@gmail.com>
2022-10-07 22:59:02 -0300

    criado metodo de busca e exclusao

--- circular_Linkedlist.py
+++ circular_Linkedlist.py
@@ -32,35 +32,93 @@ class CLinkedList:
         while(current != self.head):
             counter += 1
             current = current.next
+        
         if (position < 1 or position > (counter+1)):
            print("\nInvalid position.")
         elif (position == 1):
-            if (self.head == None):
-                self.head = newNode
-                self.head.next = self.head
-                self.head.prev = self.head
-            else:
-                while(current.next != self.head):
-                    current = current.next
-                current.next = newNode
-                newNode.prev = current
-                newNode.next = self.head
-                self.head.prev = newNode
-                self.head = newNode
+          if(self.head == None):
+            self.head = newNode
+            self.head.next = self.head
+            self.head.prev = self.head
+          else:
+            while(current.next != self.head):
+                current = current.next
+            current.next = newNode
+            newNode.prev = current
+            newNode.next = self.head
+            self.head.prev = newNode
+            self.head = newNode
             
         else: 
-            current = self.head
-            for i in range(1, position-1):
+          current = self.head
+          for i in range(1, position-1):
               current = current.next
-              newNode.next = current.next
-              newNode.next.prev = newNode
-              newNode.prev = current
-              current.next = newNode
+          newNode.next = current.next
+          newNode.next.prev = newNode
+          newNode.prev = current
+          current.next = newNode
+
+    def SearchElement(self, searchValue):
+        current = self.head
+        found = 0
+        i = 0
+
+        if(current != None):
+          while (True):
+            i += 1
+            if(current.data == searchValue):
+                found += 1
+                break
+            current = current.next
+            if(current == self.head):
+                break
+          if(found == 1):
+              print("\n",searchValue, "if found at index =", i)
+          else:
+              print("\n",searchValue, "is not found in the list.")
+        else:
+          print("The list is empty.")
+
+    def DeleteLast(self):
+        if(self.head != None):
+            if(self.head.next == self.head):
+                self.head = None
+            else:
+              current = self.head
+              while(current.next.next != self.head):
+                current = current.next
+              lastNode = current.next
+              current.next = self.head
+              self.head.prev = current
+              lastNode = None
+
+    def DeleteElement(self, delValue):
+        if(self.head == None):
+            return
+        current = self.head
+        found = False
+        while(current):
+            if(current.data == delValue):
+                found = True
+                break
+            current = current.next
+        if(found == True):
+            prevNode = current.prev
+            nextNode = current.next
+            prevNode.next = nextNode
+            nextNode.prev = prevNode
+            return   
+        else:
+            print("\nElement not found.")
+        
+           
+
+
 
     def PrintList(self):
       current = self.head
       if(current != None):
-        print("The List contains:", end=" ")
+        print("\nThe List contains:", end=" ")
         while (True):
             print(current.data, end=" ")
             current = current.next
--- main.py
+++ main.py
@@ -12,6 +12,29 @@ TheList = CLinkedList()
 TheList.addFront(10)
 TheList.addFront(20)
 TheList.addFront(30)
+TheList.addFront(40)
+
 TheList.PrintList()
+
+print("\nList after inserting at a certain position: ")
+TheList.addAtPosition(70, 2)
+TheList.PrintList()
+
+#Search an element in the list
+TheList.SearchElement(2)
+TheList.SearchElement(30)
+
+#Delete the last node
+print("\nList after deleting the last node:")
+TheList.DeleteLast()
+TheList.PrintList()
+
+print("\nList after deleting elements.")
+TheList.DeleteElement(30)
+TheList.DeleteElement(70)
+TheList.PrintList()
+
+
+
 
 
